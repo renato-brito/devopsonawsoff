c9252467303d22664646ab378f06a944d5932adf
Renato Brito <renato.brito@outlook.com>
2021-03-19 22:15:38 -0300

    teste start esteira aws

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello -> RM: 337364')
+  res.send('Hello -> RM: 337364 - teste alteração commit GIT')
 })
 
 app.listen(port, () => {
